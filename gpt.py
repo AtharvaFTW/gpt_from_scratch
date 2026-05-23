2acf2b8c98acaefaf6caa60200ac7ea15d090b92
AtharvaFTW <atharvavankhede@gmail.com>
2026-05-23 22:04:00 +0530

    add: initial gpt.py

---
+++ gpt.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+
+class TokenEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class FeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class LMHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class GPT(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
