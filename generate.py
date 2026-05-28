a11db3a9cd524cbbff4a03ee112558c53a6e5332
AtharvaFTW <atharvavankhede@gmail.com>
2026-05-28 22:53:18 +0530

    if _name_ path change

--- generate.py
+++ generate.py
@@ -5,11 +5,11 @@ import tiktoken
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 VOCAB_SIZE = 50257
-D_MODEL = 512
+D_MODEL = 256
 H = 8
-N = 6
-CONTEXT_LEN = 128
-BATCH_SIZE = 64
+N = 4
+CONTEXT_LEN = 256
+BATCH_SIZE = 256
 
 
 
