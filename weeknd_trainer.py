93969988283f78137a48d1f0efa59728c0476a99
AtharvaFTW <atharvavankhede@gmail.com>
2026-05-28 22:00:43 +0530

    reduced the hyper parameters

--- weeknd_trainer.py
+++ weeknd_trainer.py
@@ -15,10 +15,10 @@ def weeknd_training_character(data_path:str,epochs:int= 3000, device= DEVICE):
 
     # Initializing Params
     VOCAB_SIZE = vocab_size
-    D_MODEL = 1024
-    H = 16
-    N = 12
-    CONTEXT_LEN = 128
+    D_MODEL = 256
+    H = 8
+    N = 4
+    CONTEXT_LEN = 512
     BATCH_SIZE = 128
 
     model = GPT(vocab_size= VOCAB_SIZE, d_model= D_MODEL, h= H, N= N, context_len= CONTEXT_LEN).to(device)
@@ -161,6 +161,21 @@ def weeknd_training_word(data_path:str,epochs:int= 3000, device= DEVICE):
     wandb.finish()
 
 if __name__ == "__main__":
+    
     path = r"data/weeknd.txt"
     weeknd_training_character(path, epochs=3000, device = DEVICE)
+
+    # from data import get_tokens_character
+    # train_data, val_data, vocab_size, encode, decode  = get_tokens_character(path)
+
+    # # Initializing Params
+    # VOCAB_SIZE = vocab_size
+    # D_MODEL = 256
+    # H = 8
+    # N = 4
+    # CONTEXT_LEN = 128
+    # BATCH_SIZE = 128
+    # model = GPT(vocab_size= VOCAB_SIZE, d_model= D_MODEL, h= H, N= N, context_len= CONTEXT_LEN)
+    # total_params = sum(p.numel() for p in model.parameters())
+    # print(f"Parameters: {total_params:,}")
     
