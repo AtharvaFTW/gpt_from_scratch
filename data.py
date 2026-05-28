642df8452ec56efb74e72a5226579004edee48d1
AtharvaFTW <atharvavankhede@gmail.com>
2026-05-28 12:31:39 +0530

    added custom word level tokenizer

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import tiktoken
 import torch
+import string
 
 def get_tokens(path: str):
     """
@@ -25,7 +26,7 @@ def get_tokens(path: str):
 
 def get_tokens_character(path):
     """
-    This function creates token embeddigns with custom vocab for small data corpus.
+    This function creates token embeddigns at character level.
     """
 
     with open(path, "r", encoding= "utf-8") as f:
@@ -51,6 +52,40 @@ def get_tokens_character(path):
 
     return train_tensor, val_tensor , vocab_size, encode, decode
 
+def get_tokens_word(path):
+    """
+    This function creates token embeddings at word level.
+    Word level embeddings allow the model to understand better.
+    """
+
+    with open(path, "r", encoding= "utf-8") as f:
+        corpus = f.read()
+
+    corpus = corpus.lower()
+    corpus = corpus.translate(str.maketrans("", "", string.punctuation))
+
+    words = sorted(set(corpus.split()))
+    vocab_size = len(words)
+
+    w_to_idx = {w: i for i,w in enumerate(words)}
+    idx_to_w = {i: w for i,w in enumerate(words)}
+
+    def encode(text):
+        text = text.lower().translate(str.maketrans("", "", string.punctuation))
+        return [w_to_idx[w] for w in text.split()]
+
+    def decode(tokens):
+        return " ".join([idx_to_w[i] for i in tokens])
+
+    encoded_text = encode(corpus)
+    tokens = torch.tensor(encoded_text, dtype = torch.long)
+
+    train_tensor = tokens[:int((0.9) * len(tokens))]
+    val_tensor = tokens[int((0.9) * len(tokens)):]
+
+    return train_tensor, val_tensor , vocab_size, encode, decode
+
+
 
 def get_batch(data: torch.Tensor, batch_size:int, context_len:int):
     ix = torch.randint(len(data) - context_len, (batch_size,))
@@ -64,10 +99,10 @@ def get_batch(data: torch.Tensor, batch_size:int, context_len:int):
 if __name__ == "__main__":
 
     path = r"data/weeknd.txt"
-    train_tensor,val_tensor, vocab_size, encode, decode = get_tokens_character(path)
+    train_tensor,val_tensor, vocab_size, encode, decode = get_tokens_word(path)
     print(f"Vocab size: {vocab_size}")
     print(f"Train Tensor len: {len(train_tensor)}")
     print(f"Val Tensor len: {len(val_tensor)}")
-    print(f"Sample encode: {encode('Take me back to LA')}")
-    print(f"Sample decode: {decode(encode('Take me back to LA'))}")
+    print(f"Sample encode: {encode("Take me back to LA")}")
+    print(f"Sample decode: {decode(encode("Take me back to LA"))}")
     
