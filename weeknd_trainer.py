af1edc85047d5bfbc6fac09d1988c866a1c7c69d
AtharvaFTW <atharvavankhede@gmail.com>
2026-05-28 18:35:30 +0530

    updated hyperparameters

--- weeknd_trainer.py
+++ weeknd_trainer.py
@@ -90,7 +90,7 @@ def weeknd_training_word(data_path:str,epochs:int= 3000, device= DEVICE):
     H = 12
     N = 10
     CONTEXT_LEN = 256
-    BATCH_SIZE = 128
+    BATCH_SIZE = 96
 
     model = GPT(vocab_size= VOCAB_SIZE, d_model= D_MODEL, h= H, N= N, context_len= CONTEXT_LEN).to(device)
 
